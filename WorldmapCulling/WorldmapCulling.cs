e0d8d04ac967d77140c6ae08188c631401262d38
birdhimself <git@wuul.eu>
2026-02-19 00:22:44 +0100

    refactor: avoid destroying pin marker
    Instead of destroying pin markers, resize existing ones.

--- WorldmapCulling/WorldmapCulling.cs
+++ WorldmapCulling/WorldmapCulling.cs
@@ -1,10 +1,10 @@
-using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
 using BepInEx;
 using HarmonyLib;
 using Jotunn.Entities;
 using Jotunn.Managers;
-using Jotunn.Utils;
 using UnityEngine;
+// ReSharper disable InconsistentNaming
 
 namespace WorldmapCulling
 {
@@ -16,7 +16,8 @@ namespace WorldmapCulling
         public const string PluginGUID = "birdhimself.WorldmapCulling";
         public const string PluginName = "WorldmapCulling";
         public const string PluginVersion = "0.0.1";
-        
+        private const float ZoomThreshold = 0.3f;
+
         // Use this class to add your own localization to the game
         // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
         public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
@@ -25,60 +26,77 @@ namespace WorldmapCulling
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
             Jotunn.Logger.LogInfo("WorldmapCulling has landed");
-            
+
             // Register the Harmony patches
             Harmony.CreateAndPatchAll(typeof(WorldmapCulling).Assembly, PluginGUID);
         }
 
-        class MinimapUpdatePinsState
+        private class MinimapUpdatePinsState
         {
-            public float ogShowNamesZoom;
-            public float ogPinSizeLarge;
+            public float OgPinSizeLarge;
         }
-        
-        [HarmonyPatch(typeof(Minimap), nameof(Minimap.UpdatePins))]
 
-        class Minimap_UpdatePins_Patch
+        [HarmonyPatch(typeof(Minimap), nameof(Minimap.UpdatePins))]
+        [SuppressMessage("ReSharper", "UnusedMember.Local")]
+        private class Minimap_UpdatePins_Patch
         {
-            private static float zoomThreshold = 0.3f;
-            private static float rememberedPinSizeLarge = -1f;
-            
-            static void Prefix(Minimap __instance, out MinimapUpdatePinsState __state)
+            private static float _rememberedPinSizeLarge = -1f;
+
+            private static void Prefix(Minimap __instance, out MinimapUpdatePinsState __state)
             {
                 __state = new MinimapUpdatePinsState()
                 {
-                    ogShowNamesZoom = __instance.m_showNamesZoom,
-                    ogPinSizeLarge = __instance.m_pinSizeLarge,
+                    OgPinSizeLarge = __instance.m_pinSizeLarge,
                 };
 
-                if (__instance.m_mode != Minimap.MapMode.Large)
+                if (__instance.m_mode != Minimap.MapMode.Large || __instance.m_largeZoom <= ZoomThreshold)
                 {
                     return;
                 }
 
-                if (__instance.m_largeZoom <= zoomThreshold)
-                {
-                    return;
-                }
+                __instance.m_pinSizeLarge = (__instance.m_pinSizeLarge * ZoomThreshold) / __instance.m_largeZoom;
 
-                __instance.m_showNamesZoom = zoomThreshold;
-                __instance.m_pinSizeLarge = __state.ogPinSizeLarge * (1f + zoomThreshold - __instance.m_largeZoom);
-
-                if (__instance.m_pinSizeLarge != rememberedPinSizeLarge)
+                // ReSharper disable once CompareOfFloatsByEqualityOperator
+                if (__instance.m_pinSizeLarge != _rememberedPinSizeLarge)
                 {
                     foreach (var pin in __instance.m_pins)
                     {
-                        __instance.DestroyPinMarker(pin);
+                        if (pin.m_uiElement == null)
+                        {
+                            continue;
+                        }
+
+                        var size = pin.m_doubleSize ? __instance.m_pinSizeLarge * 2f : __instance.m_pinSizeLarge;
+                        pin.m_uiElement.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size);
+                        pin.m_uiElement.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size);
+
                     }
 
-                    rememberedPinSizeLarge = __instance.m_pinSizeLarge;
+                    _rememberedPinSizeLarge = __instance.m_pinSizeLarge;
                 }
             }
 
-            static void Postfix(Minimap __instance, MinimapUpdatePinsState __state)
+            private static void Postfix(Minimap __instance, MinimapUpdatePinsState __state)
             {
-                __instance.m_showNamesZoom = __state.ogShowNamesZoom;
-                __instance.m_pinSizeLarge = __state.ogPinSizeLarge;
+                __instance.m_pinSizeLarge = __state.OgPinSizeLarge;
+
+                if (__instance.m_mode != Minimap.MapMode.Large || __instance.m_largeZoom <= ZoomThreshold)
+                {
+                    return;
+                }
+
+                foreach (var pin in __instance.m_pins)
+                {
+                    if (pin.m_type is Minimap.PinType.Ping or Minimap.PinType.Player or Minimap.PinType.Shout)
+                    {
+                        continue;
+                    }
+
+                    if (pin.m_NamePinData != null && pin.m_NamePinData.PinNameGameObject != null)
+                    {
+                        pin.m_NamePinData.PinNameGameObject.SetActive(false);
+                    }
+                }
             }
         }
     }
