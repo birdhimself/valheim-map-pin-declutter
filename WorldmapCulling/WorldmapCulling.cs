701b9e3ed6d6ec7ed06695a0635644cbd0c41169
birdhimself <git@wuul.eu>
2026-02-18 20:44:29 +0100

    feat: hide names when zooming out

--- WorldmapCulling/WorldmapCulling.cs
+++ WorldmapCulling/WorldmapCulling.cs
@@ -1,7 +1,9 @@
 using BepInEx;
+using HarmonyLib;
 using Jotunn.Entities;
 using Jotunn.Managers;
 using Jotunn.Utils;
+using UnityEngine;
 
 namespace WorldmapCulling
 {
@@ -23,8 +25,25 @@ namespace WorldmapCulling
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
             Jotunn.Logger.LogInfo("WorldmapCulling has landed");
             
-            // To learn more about Jotunn's features, go to
-            // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
+            // Register the Harmony patches
+            Harmony.CreateAndPatchAll(typeof(WorldmapCulling).Assembly, PluginGUID);
+        }
+        
+        [HarmonyPatch(typeof(Minimap), nameof(Minimap.UpdateMap))]
+
+        class Minimap_UpdateMap_Patch
+        {
+            static void Prefix(Minimap __instance, Player player, float dt, bool takeInput)
+            {
+                if (__instance.m_mode != Minimap.MapMode.Large)
+                {
+                    return;
+                }
+
+                __instance.m_showNamesZoom = 0.3f;
+                
+                Jotunn.Logger.LogInfo($"We zoom {__instance.m_largeZoom} / {__instance.m_showNamesZoom}");
+            }
         }
     }
 }
