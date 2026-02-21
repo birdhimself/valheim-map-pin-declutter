82789cdf143619a00e1689d25121d8c7fb8f1abb
birdhimself <git@wuul.eu>
2026-02-21 17:23:39 +0100

    chore: bump version to 0.2.0

--- MapPinDeclutter/MapPinDeclutter.cs
+++ MapPinDeclutter/MapPinDeclutter.cs
@@ -22,7 +22,7 @@ internal class MapPinDeclutter : BaseUnityPlugin
 
     private const string PluginGUID = "birdhimself.MapPinDeclutter";
     private const string PluginName = "MapPinDeclutter";
-    public const string PluginVersion = "0.1.0";
+    public const string PluginVersion = "0.2.0";
 
     private PluginConfiguration config;
 
