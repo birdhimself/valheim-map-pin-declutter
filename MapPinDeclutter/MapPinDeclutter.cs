839877d9c9b0a5742f6aa14b5fdaa61a9d29ec86
birdhimself <git@wuul.eu>
2026-02-22 19:21:53 +0100

    chore: remove unused using directive

--- MapPinDeclutter/MapPinDeclutter.cs
+++ MapPinDeclutter/MapPinDeclutter.cs
@@ -3,7 +3,6 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using BepInEx;
-using BepInEx.Configuration;
 using HarmonyLib;
 using Jotunn.Entities;
 using Jotunn.Managers;
